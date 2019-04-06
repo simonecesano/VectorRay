b812b6817a78c49bab1b5d555858908b7d3c8d94
Simone Cesano <simonecesano@libero.it>
2019-04-06 21:46:50 +0200

    Added Catmull-Rom spline

--- libs.js
+++ libs.js
@@ -3,5 +3,6 @@ Navigo = require('navigo');
 require('svg.js')
 require('svg.draw.js')
 SVGCatmullRomSpline = require('svg-catmull-rom-spline');
+Bezier = require('bezier-js')
 
-// browserify libs.js > foo.js 
+// browserify libs.js > libs.build.js 
