7f5a9c871f12991b8f14dae4796d126e1c589928
Simone Cesano <simonecesano@libero.it>
2019-05-18 07:11:36 +0200

    Added draw, draggable, topath and more

--- libs.js
+++ libs.js
@@ -1,7 +1,15 @@
 Navigo = require('navigo');
+
 require('svg.js')
-require('svg.draw.js')
+require('svg.draw.js');
+require('svg.draggable.js')
+require('@svgdotjs/svg.topath.js')
+require('./svg.catmullRom.js')
+
 SVGCatmullRomSpline = require('svg-catmull-rom-spline');
 Bezier = require('bezier-js')
+fitCurve = require('fit-curve');
+smooth = require('chaikin-smooth')
+simplify = require('simplify-path')
 
-// browserify libs.js > libs.build.js 
+// npx webpack --config libs.config.js 
